fc5d905ee2ad47d9ad7ced5f7acd4a55eba41dba
RawailEocean <rawail.ahmed@eocean.net>
2025-07-17 03:04:49 -0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "🧹 Cleaning up old container if exists..."
+docker rm -f gpa-react-container || true
+
+echo "📦 Navigating to app directory..."
+if [ -d "gpa-react" ]; then
+  cd gpa-react && git pull origin main
+else
+  git clone https://github.com/RawailEocean/gpa-react.git
+  cd gpa-react
+fi
+
+echo "🔨 Building Docker image..."
+docker build -t gpa-react .
+
+echo "🚀 Running new Docker container..."
+docker run -d --name gpa-react-container -p 80:80 gpa-react
+
+echo "🧹 Cleaning up unused Docker images..."
+docker image prune -f
